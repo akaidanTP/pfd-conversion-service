6d9cb42d373750e52a0f11dd846befd614ab861b
Alex Kaidan <alex.kaidan@truepill.com>
2022-03-03 19:15:36 -0700

    Added load by URL option

--- src/routes/pdf.js
+++ src/routes/pdf.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { generatePDFFromHTML } from '../services/puppeteer.js'
+import { generatePDFFromHTML, generatePDFFromURL } from '../services/puppeteer.js'
 export const pdfRouter = express.Router()
 
 
@@ -7,8 +7,21 @@ export const pdfRouter = express.Router()
 pdfRouter.post('/pdf/html', async (req, res) => {
     try {
         console.log('Generating PDF')
-        console.log(req.body)
         const pdf = await generatePDFFromHTML(req.body.html)
+        console.log('Finished generating PDF')
+
+        res.set("Content-Type", "application/pdf");
+        res.send(pdf);
+    } catch (e) {
+        res.send(`Something went wrong: ${e.message}`)
+    }
+})
+
+// Should accept a post request with valid html set as "url" in the body
+pdfRouter.post('/pdf/url', async (req, res) => {
+    try {
+        console.log('Generating PDF')
+        const pdf = await generatePDFFromURL(req.body.url)
         console.log('Finished generating PDF')
 
         res.set("Content-Type", "application/pdf");
--- src/services/puppeteer.js
+++ src/services/puppeteer.js
@@ -13,3 +13,26 @@ export async function generatePDFFromHTML(html) {
 
     return pdfBuffer;
 }
+
+export async function generatePDFFromURL(url) {
+    const browser = await initializeNewBrowser()
+    const page = await browser.newPage();
+
+    // How we can set an authToken or something for security
+    // await page.setExtraHTTPHeaders({
+    //     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+    //     'upgrade-insecure-requests': '1',
+    //     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    //     'accept-encoding': 'gzip, deflate, br',
+    //     'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+    // })
+
+    await page.goto(url);
+
+    const pdfBuffer = await page.pdf();
+
+    await page.close();
+    await browser.close();
+
+    return pdfBuffer;
+}
